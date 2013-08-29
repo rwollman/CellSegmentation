6a9db1122911edede18905d56bfd570a2150530b
naomi <naomi@localhost>
2013-08-28 18:22:35 -0700

    Adding a segmentNuclei function - might still need work. Small changes to the example analysis script and to the measureCa2+ function to make them more readable - functionallity ore or less the same

--- measureMCF10ACa2.m
+++ measureMCF10ACa2.m
@@ -1,8 +1,20 @@
-function [Ca,T,CaStk] = measureMCF10ACa2(MD,well,Lbl,varargin)
+function [Ca,Tca,CaStk] = measureMCF10ACa2(MD,well,Lbl,varargin)
 
-T = MD.getSpecificMetadata('TimestampFrame','Position',well,'Channel','Red'); 
-CaStk = stkread(MD,'Position',well,'Channel','Red'); 
-% subtrack bacground (for all stack at once...); 
+%% input arguments
+arg.cachannel = 'Red'; 
+arg = parseVarargin(varargin,arg); 
+
+%% read Ca stack
+Tca = MD.getSpecificMetadata('TimestampFrame','Position',well,'Channel',arg.cachannel); 
+CaStk = stkread(MD,'Position',well,'Channel','Red','TimestampFrame',Tca); 
+
+Tca = cat(1,Tca{:});
+[Tca ,ordr]= sort(Tca);
+CaStk = CaStk(:,:,ordr); 
+
+%% subtract  bacground (for all stack at once...); 
 CaStk = backgroundSubtraction(CaStk); 
+
+%% measure intensities
 Ca = meanIntensityPerLabel(Lbl,CaStk,T,'func','median','type','nuc');
 
