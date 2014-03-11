bc084f656713080d6e727fd09c787ceda6deb16f
naomi <naomi@localhost>
2014-03-11 15:37:48 -0700

    added positiontype and cachannel arguments

--- measureMCF10ACa2.m
+++ measureMCF10ACa2.m
@@ -1,12 +1,18 @@
 function [Ca,T,CaStk] = measureMCF10ACa2(MD,well,Lbl,varargin)
 
+arg.cachannel='Red';
+arg.positiontype='Position';
 arg.timefunc = @(t) true(size(t)); 
 arg = parseVarargin(varargin,arg); 
 
 
-T = MD.getSpecificMetadata('TimestampFrame','Position',well,'Channel','Red','timefunc',arg.timefunc); 
+
+T = MD.getSpecificMetadata('TimestampFrame',arg.positiontype,well,'Channel',arg.cachannel,'timefunc',arg.timefunc); 
 T = cat(1,T{:}); 
-CaStk = stkread(MD,'Position',well,'Channel','Red','timefunc',arg.timefunc);
+CaStk = stkread(MD,arg.positiontype,well,'Channel',arg.cachannel,'timefunc',arg.timefunc);
+
+% Tca = MD.getSpecificMetadata('TimestampFrame',arg.positiontype ,well,'Channel',arg.cachannel); 
+% CaStk = stkread(MD,arg.positiontype ,well,'Channel',arg.cachannel); 
 
 %% register
 CaStk = Lbl.Reg.register(CaStk,T); 
@@ -17,4 +23,3 @@ fprintf('Finsihed subtracting background')
 
 %% do actual measurements
 Ca = meanIntensityPerLabel(Lbl,CaStk,T,'func','median','type','nuc');
-
