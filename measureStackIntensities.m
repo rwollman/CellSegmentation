7fdc286de295c2ec823b4297533bb3cfea7551ae
robertf <robertf@localhost>
2014-12-03 14:14:34 -0800

    changed from OR to AND for cytoplasm segmentation, changed disk size, and nuc_smooth, also added arg.relabel parameter

--- measureStackIntensities.m
+++ measureStackIntensities.m
@@ -1,4 +1,4 @@
-function [Ca,T,CaStk] = measureStackIntensities(MD,well,Lbl,varargin)
+function [measurement,T,measurementStack] = measureStackIntensities(MD,well,Lbl,varargin)
 
 arg.timefunc = @(t) true(size(t));
 arg.channel = ''; 
@@ -17,18 +17,18 @@ if isempty(arg.channel)
 end
 
 
-[CaStk,indx] = stkread(MD,arg.positiontype,well,'Channel',arg.channel,'timefunc',arg.timefunc,'sortby','TimestampFrame');
+[measurementStack,indx] = stkread(MD,arg.positiontype,well,'Channel',arg.channel,'timefunc',arg.timefunc,'sortby','TimestampFrame');
 T = MD.getSpecificMetadataByIndex('TimestampFrame',indx); 
 
 if iscell(T)
-    CaStk = CaStk(:,:,~cellfun(@isempty,T));
+    measurementStack = measurementStack(:,:,~cellfun(@isempty,T));
     T = cat(1,T{~cellfun(@isempty,T)});
 end
 
 
 %% register
 if arg.register && isa(Lbl.Reg,'Registration') 
-    CaStk = Lbl.Reg.register(CaStk,T);
+    measurementStack = Lbl.Reg.register(measurementStack,T);
     fprintf('Finished registration')
 end
 
@@ -36,27 +36,27 @@ end
 if arg.background
     switch arg.mskmethod
         case 'none'
-            msk = true(size(CaStk(:,:,1))); 
+            msk = true(size(measurementStack(:,:,1))); 
         case '5percentile'
-            msk = nanmean(CaStk,3);
+            msk = nanmean(measurementStack,3);
             msk = msk>prctile(msk(:),5);
         case '5percentile_eroded'
-            msk = nanmean(CaStk,3);
+            msk = nanmean(measurementStack,3);
             msk = msk>prctile(msk(:),5);
             msk = imerode(msk,strel('disk',50)); 
         otherwise
             error('Mask call for background subtractin not supported!, check for typos...')
     end
-    CaStk = backgroundSubtraction(CaStk,'msk',msk,'smoothstk',false,'smoothmethod',arg.background_smooth);
+    measurementStack = backgroundSubtraction(measurementStack,'msk',msk,'smoothstk',false,'smoothmethod',arg.background_smooth);
     fprintf('Finsihed subtracting background')
 end
 
 %% do actual measurements
 if iscell(arg.cellregiontouse)
-    Ca = cell(size(arg.cellregiontouse));
+    measurement = cell(size(arg.cellregiontouse));
     for i=1:numel(arg.cellregiontouse)
-        Ca{i} = meanIntensityPerLabel(Lbl,CaStk,T,'func','mean','type',arg.cellregiontouse{i});
+        measurement{i} = meanIntensityPerLabel(Lbl,measurementStack,T,'func','mean','type',arg.cellregiontouse{i});
     end
 else
-    Ca = meanIntensityPerLabel(Lbl,CaStk,T,'func','mean','type',arg.cellregiontouse);
+    measurement = meanIntensityPerLabel(Lbl,measurementStack,T,'func','mean','type',arg.cellregiontouse);
 end
